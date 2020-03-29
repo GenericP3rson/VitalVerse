654f9a49f237448c0ad3a5aeef1b9c9c15a3d2de
Paromita Datta <paromitadatta@shreya.lan>
2020-03-29 14:52:19 -0500

    More Health Work... Still in Beta

--- app/src/main/java/com/example/test4/MainActivity.java
+++ app/src/main/java/com/example/test4/MainActivity.java
@@ -10,16 +10,62 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 //import HealthDataStore;
-//import com.samsung.android.sdk.HealthData;
-//import com.samsung.android.sdk.*;
+//package com.samsung.android.sdk.HealthData;
+import com.samsung.android.sdk.healthdata.HealthConstants;
+import com.samsung.android.sdk.healthdata.HealthPermissionManager;
+
+import com.samsung.android.sdk.healthdata.HealthConnectionErrorResult;
+import com.samsung.android.sdk.healthdata.HealthConstants;
+import com.samsung.android.sdk.healthdata.HealthData;
+import com.samsung.android.sdk.healthdata.HealthDataResolver;
+import com.samsung.android.sdk.healthdata.HealthDataService;
+import com.samsung.android.sdk.healthdata.HealthDataStore;
+import com.samsung.android.sdk.healthdata.HealthPermissionManager;
+import com.samsung.android.sdk.healthdata.HealthResultHolder;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import android.app.Activity;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     public static final String APP_TAG = "SimpleHealth";
+    private HealthDataStore mStore;
+    private HealthConnectionErrorResult mConnError;
+    private final Set<HealthPermissionManager.PermissionKey> mKeySet;
+    private HealthResultHolder<HealthDataResolver.ReadResult> result;
+//    private final WeakReference<Activity> mInstance;
 
 //    private static MainActivity mInstance = null;
 //    private HealthDataStore mStore;
 //    private HealthConnectionErrorResult mConnError;
 //    private Set<PermissionKey> mKeySet;
+
+    MainActivity(Activity mInstance){
+//        this.mInstance = new WeakReference<>(mInstance);
+        mKeySet = new HashSet<>();
+//        mKeySet.add(new HealthPermissionManager.PermissionKey("com.samsung.shealth.step_daily_trend", HealthPermissionManager.PermissionType.READ));
+        mKeySet.add(new HealthPermissionManager.PermissionKey(HealthConstants.StepCount.HEALTH_DATA_TYPE, HealthPermissionManager.PermissionType.READ));
+        mKeySet.add(new HealthPermissionManager.PermissionKey(HealthConstants.HeartRate.HEALTH_DATA_TYPE, HealthPermissionManager.PermissionType.READ));
+//        HealthDataService healthDataService = new HealthDataService();
+
+//        try {
+//            healthDataService.initialize(mInstance);
+////            mStore = new HealthDataStore(mInstance, mConnectionListener);
+////            // Request the connection to the health data store
+////            mStore.connectService();
+//        }catch (Exception e) {
+//            e.printStackTrace();
+//        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 //        mInstance = this;
-//        mKeySet = new HashSet<PermissionKey>();
+//        mKeySet = new HashSet<>();
 //        mKeySet.add(new PermissionKey(HealthConstants.StepCount.HEALTH_DATA_TYPE, PermissionType.READ));
 //        // Create a HealthDataStore instance and set its listener
 //        mStore = new HealthDataStore(this, mConnectionListener);
@@ -70,6 +116,8 @@ public class MainActivity extends AppCompatActivity {
     public void getVitals(View v) {
         // Do something in response to button click
         // System.out.println("CLICKED");
+        System.out.println(new HealthPermissionManager.PermissionKey(HealthConstants.StepCount.HEALTH_DATA_TYPE, HealthPermissionManager.PermissionType.READ));
 
     }
+
 }
